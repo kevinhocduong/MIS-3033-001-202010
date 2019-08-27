98372dcfe2692a432569b97d7ff763169cf50d65
Kevin Duong <kevin.hoc.duong@ou.edu>
2019-08-27 16:00:05 -0500

    Creation. Completed problem 1 & 2.

---
+++ GitHub/Problem2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> FakeID = new Dictionary<int, string>();
+            FakeID.Add(911911911, "The Feds");
+        }
+    }
+}
